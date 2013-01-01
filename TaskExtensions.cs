5aef690d922d950e1396da4f11b20f40f91a13bf
Armin Sander <armin@replicator.org>
2013-01-01 19:13:02 +0100

    add TaskExtensions

---
+++ TaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Toolbox
+{
+	public static class TaskExtensions
+	{
+		public static void WaitCompletedOrCancelled(this Task task)
+		{
+			try
+			{
+				task.Wait();
+			}
+			catch (Exception)
+			{
+				if (task.IsFaulted)
+					throw;
+			}
+		}
+	}
+}
