7f24d4c8b343534922b165c26b9e8ca0c36567d0
Armin Sander <armin@replicator.org>
2013-01-17 09:37:08 +0100

    add some extension methods to avoid some nasty casts

---
+++ Casts.cs
@@ -0,0 +1,25 @@
+﻿namespace Toolbox
+{
+	public static class Casts
+	{
+		public static uint unsigned(this int value)
+		{
+			return (uint) value;
+		}
+
+		public static int signed(this uint value)
+		{
+			return (int) value;
+		}
+
+		public static ulong unsigned(this long value)
+		{
+			return (ulong) value;
+		}
+
+		public static long signed(this ulong value)
+		{
+			return (long) value;
+		}
+	}
+}
